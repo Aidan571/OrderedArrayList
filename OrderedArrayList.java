b4aba4c72dffd2bf1f29e18234f0d3d4fc8f2275
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-12-13 17:41:25 -0500

    Created second add method.

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -14,4 +14,20 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     }
     return true;
   }
+
+  public void add(int index, T element){
+    this.remove(index);
+    if(this.size() == 0){
+      super.add(element);
+    }
+    else if(element.compareTo(this.get(this.size()-1)) > 0){
+      super.add(element);
+    }
+    else
+    for(int i = 0; i < this.size(); i++){
+      if(element.compareTo(this.get(i)) < 0){
+        super.add(i,element);
+      }
+    }
+  }
 }

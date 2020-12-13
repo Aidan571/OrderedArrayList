e4c349b332abef81a9d98caa1c47dd3d8c0aa778
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-12-13 17:43:28 -0500

    Fixed second add method and created set method.

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -16,6 +16,21 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   }
 
   public void add(int index, T element){
+    if(this.size() == 0){
+      super.add(element);
+    }
+    else if(element.compareTo(this.get(this.size()-1)) > 0){
+      super.add(element);
+    }
+    else
+    for(int i = 0; i < this.size(); i++){
+      if(element.compareTo(this.get(i)) < 0){
+        super.add(i,element);
+      }
+    }
+  }
+
+  public T set(int index, T element){
     this.remove(index);
     if(this.size() == 0){
       super.add(element);
@@ -29,5 +44,6 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
         super.add(i,element);
       }
     }
+    return element;
   }
 }

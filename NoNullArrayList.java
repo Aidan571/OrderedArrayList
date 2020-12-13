940ffd1828d83d19a595921f79789bf6912ae084
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-12-13 16:26:42 -0500

    Created second add method.

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -9,4 +9,11 @@ public class NoNullArrayList<T> extends ArrayList<T>{
     super.add(e);
     return true;
   }
+  public void add(int index, T element) throws IllegalArgumentException{
+    if(element == null){
+      throw new NoSuchElementException("Can not add a null to a NoNullArrayList");
+    }
+    else
+    super.add(index,element);
+  }
 }

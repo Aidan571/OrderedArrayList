6129589d31a1514a91360befc6a4e033ae8d1524
Aidan Fingeret <aidan@Aidans-MBP.fios-router.home>
2020-12-13 23:28:22 -0500

    Fixed where return value was for add method.

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,6 +1,9 @@
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
-  public boolean add(T element){
-    if(this.size() == 0){
+  public boolean add(T element) throws IllegalArgumentException{
+    if(element == null){
+      throw new IllegalArgumentException("Can not add a null to a NoNullArrayList");
+    }
+    else if(this.size() == 0){
       super.add(element);
     }
     else if(element.compareTo(this.get(this.size()-1)) > 0){
@@ -10,13 +13,17 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     for(int i = 0; i < this.size(); i++){
       if(element.compareTo(this.get(i)) < 0){
         super.add(i,element);
+        return true;
       }
     }
     return true;
   }
 
   public void add(int index, T element){
-    if(this.size() == 0){
+    if(element == null){
+      throw new IllegalArgumentException("Can not add a null to a NoNullArrayList");
+    }
+    else if(this.size() == 0){
       super.add(element);
     }
     else if(element.compareTo(this.get(this.size()-1)) > 0){
@@ -31,6 +38,9 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   }
 
   public T set(int index, T element){
+    if(element == null){
+      throw new IllegalArgumentException("Can not add a null to a NoNullArrayList");
+    }
     this.remove(index);
     if(this.size() == 0){
       super.add(element);

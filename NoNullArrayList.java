996b12bc1c1667d59699a72327052cc2f3c931bc
Aidan Fingeret <aidan@Aidans-MBP.fios-router.home>
2020-12-13 22:52:20 -0500

    Corrected the exception I was throwing.

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,9 +1,8 @@
-import java.util.NoSuchElementException;
 import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T>{
   public boolean add(T e) throws IllegalArgumentException{
     if(e == null){
-      throw new NoSuchElementException("Can not add a null to a NoNullArrayList");
+      throw new IllegalArgumentException("Can not add a null to a NoNullArrayList");
     }
     else
     super.add(e);
@@ -11,14 +10,14 @@ public class NoNullArrayList<T> extends ArrayList<T>{
   }
   public void add(int index, T element) throws IllegalArgumentException{
     if(element == null){
-      throw new NoSuchElementException("Can not add a null to a NoNullArrayList");
+      throw new IllegalArgumentException("Can not add a null to a NoNullArrayList");
     }
     else
     super.add(index,element);
   }
   public T set(int index, T element){
     if(element == null){
-      throw new NoSuchElementException("Can not add a null to a NoNullArrayList");
+      throw new IllegalArgumentException("Can not add a null to a NoNullArrayList");
     }
     else
     super.set(index,element);
